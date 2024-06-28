81b0af104258a6075fe59178f6fac1b0f0c36afd
Tai Zeming <tai.zeming@pithos.net>
2024-06-29 01:00:03 +0800

    feat: make unfocused windows translucent

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use ipc::subscribe::subscribe;
+use ipc::{command::get_windows, subscribe::subscribe};
 use native::set_window_alpha;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let _ = subscribe("focus_changed", |payload| {
-        set_window_alpha(payload.data.focused_container.handle, 220);
+        let focused_handle = payload.data.focused_container.handle;
+
+        tokio::spawn(async move {
+            let windows = get_windows().await.unwrap();
+            windows
+                .data
+                .iter()
+                .for_each(|window| set_window_alpha(window.handle, 122));
+
+            set_window_alpha(focused_handle, 255);
+        });
+
         false
     })
     .await;
