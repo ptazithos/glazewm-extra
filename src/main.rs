c7c2b5291bc50eab7dc427ac65560ca31f1316ce
Tai Zeming <tai.zeming@pithos.net>
2024-06-29 11:52:05 +0800

    feat: accept arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use clap::{command, Parser};
 use ipc::{command::get_windows, subscribe::subscribe};
 use native::set_window_alpha;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 128)]
+    alpha: u8,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let _ = Args::parse();
+
     let _ = subscribe("focus_changed", |payload| {
+        let alpha = Args::parse().alpha;
         let focused_handle = payload.data.focused_container.handle;
 
         tokio::spawn(async move {
@@ -11,7 +22,7 @@ async fn main() {
             windows
                 .data
                 .iter()
-                .for_each(|window| set_window_alpha(window.handle, 122));
+                .for_each(|window| set_window_alpha(window.handle, alpha));
 
             set_window_alpha(focused_handle, 255);
         });
