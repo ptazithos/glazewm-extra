4965fa7b208fcc54f4d0ed3922092b30aabb96e5
Tai Zeming <tai.zeming@pithos.net>
2024-06-29 20:48:39 +0800

    refactor: use advanced GetWindowLong

--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,7 +1,7 @@
 use windows::Win32::{
     Foundation::{COLORREF, HWND},
     UI::WindowsAndMessaging::{
-        GetWindowLongA, SetLayeredWindowAttributes, SetWindowLongA, GWL_EXSTYLE, LWA_ALPHA,
+        GetWindowLongPtrW, SetLayeredWindowAttributes, SetWindowLongPtrW, GWL_EXSTYLE, LWA_ALPHA,
         WS_EX_LAYERED,
     },
 };
@@ -10,13 +10,13 @@ pub fn set_window_alpha(raw_handle: isize, alpha: u8) {
     let handle = HWND(raw_handle);
 
     unsafe {
-        let ex_style = GetWindowLongA(handle, GWL_EXSTYLE);
-        if ex_style & i32::try_from(WS_EX_LAYERED.0).unwrap() == 0 {
+        let ex_style = GetWindowLongPtrW(handle, GWL_EXSTYLE);
+        if ex_style & isize::try_from(WS_EX_LAYERED.0).unwrap() == 0 {
             println!("Setting WS_EX_LAYERED");
-            SetWindowLongA(
+            SetWindowLongPtrW(
                 handle,
                 GWL_EXSTYLE,
-                ex_style | i32::try_from(WS_EX_LAYERED.0).unwrap(),
+                ex_style | isize::try_from(WS_EX_LAYERED.0).unwrap(),
             );
         }
         let _ = SetLayeredWindowAttributes(handle, COLORREF::default(), alpha, LWA_ALPHA);
