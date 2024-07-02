d78e4c8e78783a3aa0fc2fab95f353919a03d25a
Tai Zeming <tai.zeming@pithos.net>
2024-07-02 21:45:01 +0800

    feat: init tauri project

---
+++ packages/ui/rsbuild.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  source: {
+    entry: {
+      daemon: "./src/daemon/index.tsx"
+    }
+  }
+});
