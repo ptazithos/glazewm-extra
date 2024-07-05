2787e80add62938a4b98b935040a07c2a9f3affd
Tai Zeming <tai.zeming@pithos.net>
2024-07-05 11:36:06 +0800

    chore(deps): upgrade windows

--- app/src/windows.rs
+++ app/src/windows.rs
@@ -1,3 +1,5 @@
+use std::ffi::c_void;
+
 use windows::Win32::{
     Foundation::{COLORREF, HWND, RECT},
     UI::WindowsAndMessaging::{
@@ -9,7 +11,7 @@ use windows::Win32::{
 
 #[tauri::command]
 pub fn set_window_alpha(raw_handle: isize, alpha: u8) {
-    let handle = HWND(raw_handle);
+    let handle = HWND(raw_handle as *mut c_void);
 
     unsafe {
         let ex_style = GetWindowLongPtrW(handle, GWL_EXSTYLE);
@@ -26,7 +28,7 @@ pub fn set_window_alpha(raw_handle: isize, alpha: u8) {
 
 #[tauri::command]
 pub fn set_window_titlebar(raw_handle: isize, titlebar: bool) {
-    let handle = HWND(raw_handle);
+    let handle = HWND(raw_handle as *mut c_void);
 
     unsafe {
         let ex_style = GetWindowLongPtrW(handle, GWL_STYLE);
