bbca4fd6bb40acb1affdaadc75641eb75e06273e
Tai Zeming <tai.zeming@pithos.net>
2024-07-11 21:35:28 +0800

    fix(overview): hide window name

--- src/overview/components/window-view.tsx
+++ src/overview/components/window-view.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import type { WindowSpace } from "../../ipc/command";
 import type { Optional } from "../../ipc/utils";
 
 const WindowView = (props: { window: Optional<WindowSpace> }) => {
+	const container = useRef<HTMLDivElement>(null);
+	const text = useRef<HTMLSpanElement>(null);
+	const [showText, setShowText] = useState(false);
+
+	useLayoutEffect(() => {
+		const textWidth = text.current?.offsetWidth ?? 0;
+		const containerWidth = container.current?.offsetWidth ?? 0;
+
+		setShowText(containerWidth >= textWidth);
+	}, []);
+
 	return (
-		<div className="flex-1 border-gray border-solid border-2 rounded-xl flex justify-center items-center">
-			<span className="text-gray">Window</span>
+		<div
+			ref={container}
+			className="flex-1 border-gray border-solid border-2 rounded-xl flex justify-center items-center"
+		>
+			<span
+				ref={text}
+				className={`absolute text-gray ${showText ? "" : "invisible"}`}
+			>
+				Windows
+			</span>
 		</div>
 	);
 };
