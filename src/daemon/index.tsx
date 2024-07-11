e668b134505d058dbbb643997465e3199e59d426
Tai Zeming <tai.zeming@pithos.net>
2024-07-11 14:18:21 +0800

    feat: cleanup service

--- src/daemon/index.tsx
+++ src/daemon/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { invoke } from "@tauri-apps/api/tauri";
-import { listen } from "@tauri-apps/api/event";
-import { exit } from "@tauri-apps/api/process";
+
 import TitleService from "./services/titile";
-import type { AppConfig } from "../ipc/utils";
 import AlphaService from "./services/alpha";
-import { getWindows } from "../ipc/command";
+import CleanUpService from "./services/cleanup";
+
+import type { AppConfig } from "../ipc/utils";
 
 const DaemonApp = () => {
 	const [appConfig, setAppConfig] = useState<AppConfig | null>(null);
@@ -15,23 +15,6 @@ const DaemonApp = () => {
 		const init = async () => {
 			const appConfig = await invoke<AppConfig>("get_app_config");
 			setAppConfig(appConfig);
-
-			await listen("clean_quit", async () => {
-				const windows = await getWindows();
-				for (const window of windows) {
-					await invoke("set_window_alpha", {
-						rawHandle: window.handle,
-						alpha: 255,
-					});
-
-					await invoke("set_window_titlebar", {
-						rawHandle: window.handle,
-						titlebar: true,
-					});
-				}
-
-				await exit(1);
-			});
 		};
 
 		init();
@@ -41,6 +24,7 @@ const DaemonApp = () => {
 		<>
 			<TitleService config={appConfig} />
 			<AlphaService config={appConfig} />
+			<CleanUpService />
 		</>
 	) : (
 		<></>
