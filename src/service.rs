8b82db9fbbc580b9b6824c861be07748bfee33fb
Tai Zeming <tai.zeming@pithos.net>
2024-07-24 21:06:56 +0800

    feat: handle parse error

--- src/service.rs
+++ src/service.rs
@@ -2,7 +2,7 @@ use std::future::Future;
 
 use serde_json::Value;
 use tokio::select;
-use tracing::info;
+use tracing::{error, info};
 
 use crate::config::AppConfig;
 
@@ -27,24 +27,29 @@ impl<T: EventRegistry> EffectService<T> {
 
     fn setup_ipc_callbacks(&mut self) {
         self.ipc.register(|msg| {
-            let payload: Value = serde_json::from_str(msg).unwrap();
-            if let Some(response_type) = payload["data"]["type"].as_str() {
-                match response_type {
-                    "focus_changed" => {
-                        if let Some(hwnd) = &payload["data"]["focusedContainer"]["handle"].as_i64()
-                        {
-                            info!("Focused window handle: {}", hwnd);
+            if let Ok(payload) = serde_json::from_str(msg) as Result<Value, _> {
+                if let Some(response_type) = payload["data"]["type"].as_str() {
+                    match response_type {
+                        "focus_changed" => {
+                            if let Some(hwnd) =
+                                &payload["data"]["focusedContainer"]["handle"].as_i64()
+                            {
+                                info!("Focused window handle: {}", hwnd);
+                            }
                         }
-                    }
-                    "window_managed" => {
-                        if let Some(hwnd) = &payload["data"]["managedWindow"]["handle"].as_i64() {
-                            info!("Managed window handle: {}", hwnd);
+                        "window_managed" => {
+                            if let Some(hwnd) = &payload["data"]["managedWindow"]["handle"].as_i64()
+                            {
+                                info!("Managed window handle: {}", hwnd);
+                            }
                         }
-                    }
-                    _ => {
-                        info!("Unknown response {}", msg);
+                        _ => {
+                            info!("Unknown response {}", msg);
+                        }
                     }
                 }
+            } else {
+                error!("Failed to parse message: {}", msg);
             }
         });
     }
