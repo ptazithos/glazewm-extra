5cc68a9ba8f4250a2415b31bb6bd9c2720a02a25
Tai Zeming <tai.zeming@pithos.net>
2024-07-25 17:07:14 +0800

    feat: parse rule & apply rule

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,11 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::fs;
 
+use crate::windows::{get_window_info, set_window_alpha, set_window_rounded, set_window_titlebar};
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct WindowRule {
+pub struct RawWindowRule {
     pub command: String,
     pub match_process_name: Option<String>,
     pub match_class_name: Option<String>,
@@ -10,41 +13,194 @@ pub struct WindowRule {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct AppConfig {
-    pub window_rules: Option<Vec<WindowRule>>,
-    pub focused_window_rules: Option<Vec<WindowRule>>,
-    pub unfocused_window_rules: Option<Vec<WindowRule>>,
+pub struct Config {
+    pub window_rules: Option<Vec<RawWindowRule>>,
+    pub focused_window_rules: Option<Vec<RawWindowRule>>,
+    pub unfocused_window_rules: Option<Vec<RawWindowRule>>,
 }
 
-impl Default for AppConfig {
+impl Default for Config {
     fn default() -> Self {
-        AppConfig {
+        Config {
             window_rules: Some(vec![
-                WindowRule {
+                RawWindowRule {
                     command: "set title false".to_string(),
                     match_process_name: Some(".*".to_string()),
                     match_class_name: None,
                     match_title: None,
                 },
-                WindowRule {
+                RawWindowRule {
                     command: "set rounded false".to_string(),
                     match_process_name: Some(".*".to_string()),
                     match_class_name: None,
                     match_title: None,
                 },
             ]),
-            focused_window_rules: Some(vec![WindowRule {
+            focused_window_rules: Some(vec![RawWindowRule {
                 command: "set translucent 255".to_string(),
                 match_process_name: Some(".*".to_string()),
                 match_class_name: None,
                 match_title: None,
             }]),
-            unfocused_window_rules: Some(vec![WindowRule {
+            unfocused_window_rules: Some(vec![RawWindowRule {
                 command: "set translucent 220".to_string(),
                 match_process_name: Some(".*".to_string()),
                 match_class_name: None,
                 match_title: None,
             }]),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum CommandType {
+    Set,
+}
+
+#[derive(Debug, Clone)]
+enum CommandCategory {
+    Translucent(u8),
+    Titlebar(bool),
+    Rounded(bool),
+}
+
+#[derive(Debug, Clone)]
+pub struct WindowRule {
+    command: (CommandType, CommandCategory),
+    match_process_name: Option<String>,
+    match_class_name: Option<String>,
+    match_title: Option<String>,
+}
+
+impl WindowRule {
+    fn from_raw(rule: &RawWindowRule) -> Option<Self> {
+        let elements = rule.command.split_whitespace().collect::<Vec<&str>>();
+        if let Some(command_type) = elements.get(0) {
+            if *command_type == "set" {
+                if let Some(category) = elements.get(1) {
+                    match *category {
+                        "translucent" => {
+                            let value = elements
+                                .get(2)
+                                .unwrap_or(&"255")
+                                .parse::<u8>()
+                                .unwrap_or(255);
+
+                            Some(WindowRule {
+                                command: (CommandType::Set, CommandCategory::Translucent(value)),
+                                match_class_name: rule.match_class_name.clone(),
+                                match_process_name: rule.match_process_name.clone(),
+                                match_title: rule.match_title.clone(),
+                            })
+                        }
+                        "title" => {
+                            let value = elements
+                                .get(2)
+                                .unwrap_or(&"true")
+                                .parse::<bool>()
+                                .unwrap_or(true);
+
+                            Some(WindowRule {
+                                command: (CommandType::Set, CommandCategory::Titlebar(value)),
+                                match_class_name: rule.match_class_name.clone(),
+                                match_process_name: rule.match_process_name.clone(),
+                                match_title: rule.match_title.clone(),
+                            })
+                        }
+                        "rounded" => {
+                            let value = elements
+                                .get(2)
+                                .unwrap_or(&"true")
+                                .parse::<bool>()
+                                .unwrap_or(true);
+
+                            Some(WindowRule {
+                                command: (CommandType::Set, CommandCategory::Rounded(value)),
+                                match_class_name: rule.match_class_name.clone(),
+                                match_process_name: rule.match_process_name.clone(),
+                                match_title: rule.match_title.clone(),
+                            })
+                        }
+                        _ => None,
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+    pub fn apply(&self, hwnd: isize) {
+        let window_info = get_window_info(hwnd);
+
+        let is_title_matched = self.match_title.clone().map_or(false, |match_title| {
+            Regex::new(&match_title)
+                .map(|re| re.is_match(&window_info.title))
+                .unwrap_or(false)
+        });
+
+        let is_class_name_matched =
+            self.match_class_name
+                .clone()
+                .map_or(false, |match_class_name| {
+                    Regex::new(&match_class_name)
+                        .map(|re| re.is_match(&window_info.class))
+                        .unwrap_or(false)
+                });
+
+        let is_process_name_matched =
+            self.match_process_name
+                .clone()
+                .map_or(false, |match_process_name| {
+                    Regex::new(&match_process_name)
+                        .map(|re| re.is_match(&window_info.process_name))
+                        .unwrap_or(false)
+                });
+
+        if is_title_matched | is_class_name_matched | is_process_name_matched {
+            match self.command.1 {
+                CommandCategory::Translucent(value) => set_window_alpha(hwnd, value),
+                CommandCategory::Titlebar(value) => set_window_titlebar(hwnd, value),
+                CommandCategory::Rounded(value) => set_window_rounded(hwnd, value),
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AppConfig {
+    pub window_rules: Vec<WindowRule>,
+    pub focused_window_rules: Vec<WindowRule>,
+    pub unfocused_window_rules: Vec<WindowRule>,
+}
+
+impl AppConfig {
+    fn from_config(config: &Config) -> Self {
+        AppConfig {
+            window_rules: config
+                .window_rules
+                .clone()
+                .unwrap_or_default()
+                .into_iter()
+                .filter_map(|rule| WindowRule::from_raw(&rule))
+                .collect(),
+            focused_window_rules: config
+                .focused_window_rules
+                .clone()
+                .unwrap_or_default()
+                .into_iter()
+                .filter_map(|rule| WindowRule::from_raw(&rule))
+                .collect(),
+            unfocused_window_rules: config
+                .unfocused_window_rules
+                .clone()
+                .unwrap_or_default()
+                .into_iter()
+                .filter_map(|rule| WindowRule::from_raw(&rule))
+                .collect(),
         }
     }
 }
@@ -58,16 +214,16 @@ pub fn parse_config() -> AppConfig {
         Ok(_) => {
             let config_str = fs::read_to_string(&config_path).unwrap();
 
-            if let Ok(app_config) = toml::from_str::<AppConfig>(&config_str) {
-                app_config
+            if let Ok(config) = toml::from_str::<Config>(&config_str) {
+                AppConfig::from_config(&config)
             } else {
-                AppConfig::default()
+                AppConfig::from_config(&Config::default())
             }
         }
         Err(_) => {
-            let config_str = toml::to_string(&AppConfig::default()).unwrap();
+            let config_str = toml::to_string(&Config::default()).unwrap();
             let _ = fs::write(&config_path, config_str);
-            AppConfig::default()
+            AppConfig::from_config(&Config::default())
         }
     }
 }
--- src/service.rs
+++ src/service.rs
@@ -1,17 +1,10 @@
 use std::future::Future;
 
-use regex::Regex;
 use serde_json::Value;
 use tokio::select;
 use tracing::{error, info};
 
-use crate::{
-    config::{AppConfig, WindowRule},
-    windows::{
-        get_visible_windows, get_window_info, set_window_alpha, set_window_rounded,
-        set_window_titlebar,
-    },
-};
+use crate::{config::AppConfig, windows::get_visible_windows};
 
 pub trait EventRegistry {
     fn register<F>(&mut self, closure: F)
@@ -80,125 +73,21 @@ impl<T: EventRegistry> EffectService<T> {
 }
 
 fn handle_focus_change(config: &AppConfig, focused_hwnd: isize) {
-    let focused_rules = match &config.focused_window_rules {
-        Some(value) => value,
-        None => &Vec::new(),
-    };
-
-    let blur_rules = match &config.unfocused_window_rules {
-        Some(value) => value,
-        None => &Vec::new(),
-    };
-
     let windows = get_visible_windows();
 
     windows.iter().for_each(|hwnd| {
         if *hwnd == focused_hwnd {
-            focused_rules.iter().for_each(|rule| {
-                match_window_rule(rule, *hwnd);
+            config.focused_window_rules.iter().for_each(|rule| {
+                rule.apply(*hwnd);
             });
         } else {
-            blur_rules.iter().for_each(|rule| {
-                match_window_rule(rule, *hwnd);
+            config.unfocused_window_rules.iter().for_each(|rule| {
+                rule.apply(*hwnd);
             });
         }
     });
 }
 
 fn handle_window_managed(config: &AppConfig, hwnd: isize) {
-    let managed_rules = match &config.window_rules {
-        Some(value) => value,
-        None => &Vec::new(),
-    };
-
-    managed_rules.iter().for_each(|rule| {
-        match_window_rule(rule, hwnd);
-    });
-}
-
-fn match_window_rule(rule: &WindowRule, hwnd: isize) {
-    let window_info = get_window_info(hwnd);
-
-    if let Some(match_title) = &rule.match_title {
-        let re = Regex::new(match_title).unwrap();
-
-        if re.is_match(&window_info.title) {
-            info!("Matched rule '{}' for window {}", match_title, hwnd);
-            apply_window_effect(rule, hwnd);
-        } else {
-            info!(
-                "Skipping rule for window {} because title does not match",
-                hwnd
-            );
-        }
-        return;
-    }
-
-    if let Some(match_class) = &rule.match_class_name {
-        let re = Regex::new(match_class).unwrap();
-
-        if re.is_match(&window_info.class) {
-            info!("Matched rule '{}' for window {}", match_class, hwnd);
-            apply_window_effect(rule, hwnd);
-        } else {
-            info!(
-                "Skipping rule for window {} because class does not match",
-                hwnd
-            );
-        }
-        return;
-    }
-
-    if let Some(match_process) = &rule.match_process_name {
-        let re = Regex::new(match_process).unwrap();
-
-        if re.is_match(&window_info.process_name) {
-            info!("Matched rule '{}' for window {}", match_process, hwnd);
-            apply_window_effect(rule, hwnd);
-        } else {
-            info!(
-                "Skipping rule for window {} because process does not match",
-                hwnd
-            );
-        }
-        return;
-    }
-}
-
-fn apply_window_effect(rule: &WindowRule, hwnd: isize) {
-    let rule_elements = rule.command.split_whitespace().collect::<Vec<&str>>();
-    if let Some(kind) = rule_elements.get(1) {
-        match *kind {
-            "translucent" => {
-                let value = rule_elements
-                    .get(2)
-                    .unwrap_or(&"255")
-                    .parse::<u8>()
-                    .unwrap_or(255);
-                set_window_alpha(hwnd, value);
-                info!("Setting window {} to translucent {}", hwnd, value);
-            }
-            "title" => {
-                let value = rule_elements
-                    .get(2)
-                    .unwrap_or(&"true")
-                    .parse::<bool>()
-                    .unwrap_or(true);
-                set_window_titlebar(hwnd, value);
-                info!("Setting window {} title to {}", hwnd, value);
-            }
-            "rounded" => {
-                let value = rule_elements
-                    .get(2)
-                    .unwrap_or(&"true")
-                    .parse::<bool>()
-                    .unwrap_or(true);
-                info!("Setting window {} rounded to {}", hwnd, value);
-                set_window_rounded(hwnd, value)
-            }
-            _ => {
-                error!("Unknown rule: {}", rule.command);
-            }
-        }
-    }
+    config.window_rules.iter().for_each(|rule| rule.apply(hwnd))
 }
